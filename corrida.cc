8107c48d1ce2ce0c314370c732ef78ef62d3895f
NestorRaul <31254585+NestorRaul@users.noreply.github.com>
2017-10-27 13:57:32 -0500

    Add files via upload
    Trabajo de la clase del dia viernes 27 de octubre
    Primera parte de la tarea

---
+++ corrida.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int esprimo(int x){
+ int flag=0;
+ for(int i=2;i<=x/2; i++)
+   {
+    if(x%i==0)
+    flag=1;
+   }
+ return flag;
+}
+int main(){
+ int p=0;
+ cout<<"Dame un numero"<<endl;
+ cin>>p;
+ do{
+   if(p%2==0)
+      p=p+1;
+   if(esprimo(p)==1)
+     p=p+2;
+ } while (esprimo(p)==1);
+   cout<<"el valor de p es"<<p<<endl;
+ return 0;
+}
